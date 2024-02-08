a339db2fb5742211889d8179415bef5742137b10
srundara <dara.srun168@gmail.com>
2024-02-08 15:18:58 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import TopNav from './views/nav/TopNav.vue'
+</script>
+
+<template>
+  <div class="">
+    <top-nav/>
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/nav/menu.js
@@ -0,0 +1,33 @@
+import {ref} from 'vue'
+export const topMenu = ref(
+    {
+        leftMenu:[
+            {
+                "id":"1",
+                "name":"Daily Deals"
+            },
+            {
+                "id":"2",
+                "name":"Help & Contact"
+            }
+        ],
+        rightMenu:[
+            {
+                "id":"1",
+                "name":"Ship to"
+            },
+            {
+                "id":"2",
+                "name":"Sell"
+            },
+            {
+                "id":"3",
+                "name":"Watch List"
+            },
+            {
+                "id":"4",
+                "name":"My eBay"
+            }
+        ]
+    }
+)
