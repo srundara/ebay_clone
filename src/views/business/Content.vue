c0e61dd74d3a72b84c4e7895678789420d80bd99
srundara <dara.sun168@gmail.com.com>
2024-02-13 00:32:34 +0700

    update

---
+++ src/views/business/Content.vue
@@ -0,0 +1,72 @@
+<script setup>
+import {ref} from 'vue'
+import {imgHeader,topBrandImg,bottomImg,imgShow,brandShow} from './business' 
+import { favoriteBrands } from '../nav/menu'
+import { RouterLink , RouterView } from 'vue-router'
+const isActive = ref(true)
+import Detail from './Detail.vue'
+</script>
+<template>
+    <div class="col-lg-10">
+        <div class="row">
+            <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 ">
+                <div class="row">
+                    <div class="top-img-contant">
+                        <RouterLink to="">
+                            <img :src="imgHeader" alt="">
+                        </RouterLink>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-4 box-img" v-for="(img,i) in topBrandImg.imgList" :key="i">
+                        <RouterLink to="">
+                            <img :src="img.img" alt="">
+                        </RouterLink>
+                    </div>
+                </div>
+                <div class="branch_box">
+                    <div class="row mt-2 ">
+                        <h4>Shop By Category</h4>
+                        <div class="branch-show " v-for="(item,i) in brandShow.show" :key="i">
+                            <div class="imgBranch">
+                                <RouterLink to="">
+                                    <img :src="item.img" alt="">
+                                    {{ item.name }}
+                                </RouterLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="branch_box_img">
+                    <div class="row mt-2 ">
+                        <h4>Shop By Branch</h4>
+                        <div class="branch-show " v-for="(item,i) in favoriteBrands.brands" :key="i" v-show="item.isActive">
+                            <div class="imgBranch" v-if="item.isActive">
+                                <RouterLink to="">
+                                    <img :src="item.img" alt="">
+                                    {{item.name}}
+                                </RouterLink>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-lg-4 box-img" v-for="(img,i) in bottomImg.imgList" :key="i">
+                        <RouterLink to="">
+                            <img :src="img.img" alt="">
+                        </RouterLink>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6 box-img" v-for="(img,i) in imgShow.img" :key="i">
+                        <RouterLink to="">
+                            <img :src="img.img" alt="">
+                        </RouterLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Detail/>
+    </div>
+    
+</template>
---
+++ src/views/business/Detail.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import {ref} from 'vue';
+    const con1 = "<p>From <RouterLink to=''>smartzpanhones</RouterLink> and <RouterLink to=''>laptops</RouterLink> to <RouterLink to=''>cameras</RouterLink>, <RouterLink to=''>TVs</RouterLink> and <RouterLink to=''>Video Game consoles</RouterLink> you can find the hottest electronics on eBay. Sellers offer a huge selection of brand new products and collector's items, many of which are difficult to find anywhere else. Pair that with competitive pricing and you've got the ultimate online marketplace for electronics</p>"
+</script>
+<template>
+    <div class="col-md-12 txt_box">
+        <h2>Electronics on eBay</h2>
+        <p>From <RouterLink to="">smartphones</RouterLink>and <RouterLink to="">laptops</RouterLink> to <RouterLink to="">cameras</RouterLink>, <RouterLink to="">TVs</RouterLink> and <RouterLink to="">video game consoles</RouterLink> you can find the hottest electronics on eBay.</p>
+        <p>Sellers offer a huge selection of brand new products and collector's items, many of which are difficult to find anywhere else. Pair that with competitive pricing and you've got the ultimate online marketplace for electronics.</p>
+    </div>
+</template>
