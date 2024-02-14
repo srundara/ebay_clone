6e7d9d650465f2d264401b3b9e37d58a99aa817e
srundara <dara.srun168@gmail.com>
2024-02-14 11:42:32 +0700

    update sport left bar

--- src/views/sport/SportLeftBar.vue
+++ src/views/sport/SportLeftBar.vue
@@ -6,36 +6,19 @@ import { sportMenu} from "../nav/menu";
 <template>
     <div class="col-lg-2">
         <div class="shopby-Category">
-                <div class="box_sport_menu">
-                    <h5 class="">Shop by Category</h5>
-                    <ul>
-                        <li v-for="(item,i) in sportMenu.leftMenu" :key="i">
-                            <RouterLink to="">{{item.name}} <i class="fa-solid fa-chevron-down"></i></RouterLink>
-                            
-                            <ul class="subMenu">
-                                <li v-for="(item,i) in item.subMenu" :key="i">
-                                    <RouterLink to="">{{item.name}}</RouterLink>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                    
-                </div>
-                <!-- <div class="box">
-                    <h5 class="">Your Favorite Brands</h5>
-                    <li v-for="(item,i) in favoriteBrands.brands" :key="i">
-                        <RouterLink to="">{{item.name}}</RouterLink>
+            <div class="box_sport_menu">
+                <h5 class="">Shop by Category</h5>
+                <ul>
+                    <li v-for="(item,i) in sportMenu.leftMenu" :key="i">
+                        <RouterLink to=""><span>{{item.name}}</span> <i class="fa-solid fa-chevron-down"></i></RouterLink>
+                        <ul class="subMenu">
+                            <li v-for="(item,i) in item.subMenu" :key="i">
+                                <RouterLink to="">{{item.name}}</RouterLink>
+                            </li>
+                        </ul>
                     </li>
-
-                </div>
-                <div class="box">
-                    <h5 class="">Shop by Interest</h5>
-                    <li v-for="(item,i) in shopByInterest.byInterest" :key="i">
-                        <RouterLink to="">{{item.name}}</RouterLink>
-                    </li>
-                </div>         -->
-            <!-- </ul> -->
+                </ul>
+            </div>
         </div>
-
     </div>
 </template>
